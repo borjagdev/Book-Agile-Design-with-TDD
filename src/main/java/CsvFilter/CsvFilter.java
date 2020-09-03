6f119757877d0a8dbd1aed8a105fc0ad44316a95
Borja <borjagarcia.dev@gmail.com>
2020-09-03 13:17:06 +0100

    CsvFilter: Both tax fields empty Test Passing

--- src/main/java/CsvFilter/CsvFilter.java
+++ src/main/java/CsvFilter/CsvFilter.java
@@ -8,7 +8,8 @@ public class CsvFilter {
         List<String> filteredContent = new ArrayList<>();
         filteredContent.add(originalContent.get(0));
         String[] invoiceElements = originalContent.get(1).split(",");
-        if (invoiceElements[4].isEmpty() || invoiceElements[5].isEmpty()) {
+        if ((invoiceElements[4].isEmpty() || invoiceElements[5].isEmpty()) &&
+                !(invoiceElements[4].isEmpty() && invoiceElements[5].isEmpty())) {
             filteredContent.add(originalContent.get(1));
         }
         return filteredContent;
--- src/test/java/CsvFilterShould.java
+++ src/test/java/CsvFilterShould.java
@@ -46,4 +46,19 @@ class CsvFilterShould {
         assertThat(result).isEqualTo(expectedResult);
     }
 
+    @Test
+    void exclude_lines_with_both_tax_fields_empty_as_one_is_required() {
+        List<String> csvContent = new ArrayList<>();
+        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        String invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,";
+        csvContent.add(headerLine);
+        csvContent.add(invoiceLine);
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add(headerLine);
+
+        List<String> result = CsvFilter.filter(csvContent);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
 }
