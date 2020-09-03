520782e0c4af8b74b75919db1e36eda5c32cf1ed
Borja <borjagarcia.dev@gmail.com>
2020-09-03 13:04:31 +0100

    CsvFilter: Both tax fields Test Passing

--- src/main/java/CsvFilter/CsvFilter.java
+++ src/main/java/CsvFilter/CsvFilter.java
@@ -1,9 +1,16 @@
 package CsvFilter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CsvFilter {
     public static List<String> filter(List<String> originalContent) {
-        return originalContent;
+        List<String> filteredContent = new ArrayList<>();
+        filteredContent.add(originalContent.get(0));
+        String[] invoiceElements = originalContent.get(1).split(",");
+        if (invoiceElements[4].isEmpty() || invoiceElements[5].isEmpty()) {
+            filteredContent.add(originalContent.get(1));
+        }
+        return filteredContent;
     }
 }
--- src/test/java/CsvFilterShould.java
+++ src/test/java/CsvFilterShould.java
@@ -7,12 +7,12 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /* TODO:
- - A file with a single bill where everything is correct should output the same line
- - A file with a single bill where IVA and IGIC (exclusive taxes) are filled should delete the line
- - A file with a single bill where the net cost is wrong calculated should delete the line
- - A file with a single bill where CIF and NIF are filled should delete the line
+ - A file with a single invoice where everything is correct should output the same line
+ - A file with a single invoice where IVA and IGIC (exclusive taxes) are filled should delete the line
+ - A file with a single invoice where the net cost is wrong calculated should delete the line
+ - A file with a single invoice where CIF and NIF are filled should delete the line
  - A file with just 1 line is not valid because it has no header
- - If the bill number is repeated, all the lines where it appears should be deleted
+ - If the invoice number is repeated, all the lines where it appears should be deleted
  - An empty or null list should output an empty list
 */
 
@@ -29,6 +29,21 @@ class CsvFilterShould {
         List<String> result = CsvFilter.filter(csvContent);
 
         assertThat(result).isEqualTo(csvContent);
+    }
+
+    @Test
+    void exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive() {
+        List<String> csvContent = new ArrayList<>();
+        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        String invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,";
+        csvContent.add(headerLine);
+        csvContent.add(invoiceLine);
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add(headerLine);
+
+        List<String> result = CsvFilter.filter(csvContent);
+
+        assertThat(result).isEqualTo(expectedResult);
     }
 
 }
